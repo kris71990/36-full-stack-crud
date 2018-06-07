3e99989363978588e4a315be6293f7bccffb81d8
Kris Sakarias <ksakarias@gmail.com>
2018-06-06 17:47:45 -0700

    added cors to server

--- front-end/src/actions/authActions.js
+++ front-end/src/actions/authActions.js
@@ -20,6 +20,7 @@ export const logout = () => {
 export const signupRequest = user => (store) => {
   return superagent.post(`${API_URL}${routes.SIGNUP_ROUTE}`)
     .send(user)
+    .withCredentials()
     .then((response) => {
       return store.dispatch(setToken(response.text));
     });
@@ -28,6 +29,7 @@ export const signupRequest = user => (store) => {
 export const loginRequest = user => (store) => {
   return superagent.get(`${API_URL}${routes.LOGIN_ROUTE}`)
     .auth(user.username, user.password)
+    .withCredentials()
     .then((response) => {
       return store.dispatch(setToken(response.text));
     });
