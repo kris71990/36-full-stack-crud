11f6c03e489b668cd601206fdc443a64b9c2fb91
Kris Sakarias <ksakarias@gmail.com>
2018-06-12 12:30:32 -0700

    cleanup from class

--- front-end/src/actions/pictureActions.js
+++ front-end/src/actions/pictureActions.js
@@ -1,24 +1,24 @@
 import superagent from 'superagent';
 import * as routes from '../utils/routes';
 
-const setPicture = pictures => ({
-  type: 'PICTURES_SET',
-  payload: pictures,
-});
+// const setPicture = pictures => ({
+//   type: 'PICTURES_SET',
+//   payload: pictures,
+// });
 
 const createPicture = picture => ({
   type: 'PICTURE_CREATE',
   payload: picture,
 });
 
-const createRequestPicture = picture => (store) => {
+const createRequestPicture = file => (store) => {
   const { token } = store.getState();
   // if doing for dogs, no token required
 
   return superagent.post(`${API_URL}/${routes.PICTURE_ROUTE}`)
     .set('Authorization', `Bearer ${token}`)
-    .field('description', picture.description)
-    .attach('picture', picture.photo)
+    .field('description', file.description)
+    .attach('picture', file.picture)
     .then((response) => {
       return store.dispatch(createPicture(response.body));
     });
